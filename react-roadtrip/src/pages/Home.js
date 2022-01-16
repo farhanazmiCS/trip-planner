c3312bedd0fa17af5ad8287dccb9bd6b27fbee9e
Farhan Azmi <farhanazmi177@gmail.com>
2022-01-16 19:13:24 +0800

    Fixed routing issue.

--- react-roadtrip/src/pages/Home.js
+++ react-roadtrip/src/pages/Home.js
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router';
 
 // Loads "Trips" or "Login" pages depending on the auth status
 export default function Home(props) {
-    if (props.isLoggedIn) {
+    if (sessionStorage.getItem(sessionStorage.getItem('username')) !== null) {
         return (
             <Navigate to="/trips" />
         )
