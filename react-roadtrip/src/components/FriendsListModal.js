64ae88d7f90f7075b2222d525ce11d2dfd7a5c67
Farhan Azmi <farhanazmi177@gmail.com>
2022-02-15 16:47:44 +0800

    Style change to code.

--- react-roadtrip/src/components/FriendsListModal.js
+++ react-roadtrip/src/components/FriendsListModal.js
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import Modal from 'react-bootstrap/Modal';
 import { useNavigate } from 'react-router-dom';
 
